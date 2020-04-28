8142766d7b6dadb519fb1e2a03d161f75a20673b
Talita Azevedo <talita.azevedo360@gmail.com>
2020-04-28 13:49:01 -0300

    Estilos Globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => <h1>App</h1>;
+const App: React.FC = () => (
+  <>
+    <h1>App</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
