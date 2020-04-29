f64f457dc386ddb64f14db019ece62c787ef6e9a
Talita Azevedo <talita.azevedo360@gmail.com>
2020-04-29 15:05:57 -0300

    Validação de Login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,38 +1,70 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+
 import logoImg from '../../assets/logo.svg';
-import { Container, Content, Background } from './styles';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignIn: React.FC = () => (
-  <>
-    <Container>
-      <Content>
-        <img src={logoImg} alt="GoBarber" />
+import { Container, Content, Background } from './styles';
+import getValidationErrors from '../../utils/getValidationErrors';
 
-        <form action="">
-          <h1>Faça Seu logon</h1>
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="Senha"
-          />
-          <Button type="submit">Entrar</Button>
-          <a href="#asda"> Esqueci minha senha</a>
-        </form>
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  console.log(formRef);
 
-        <a href="http://">
-          <FiLogIn />
-          Criar Conta
-        </a>
-      </Content>
-      <Background />
-    </Container>
-  </>
-);
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email('Digite um e-mail válido')
+          .required('E-mail obrigatório'),
+        password: Yup.string().min(6, 'Senha Obrigatória'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+
+      // ? significa que num primeiro momento o formRef é nulo
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <Content>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça Seu logon</h1>
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Senha"
+            />
+            <Button type="submit">Entrar</Button>
+            <a href="#asda"> Esqueci minha senha</a>
+          </Form>
+
+          <a href="http://">
+            <FiLogIn />
+            Criar Conta
+          </a>
+        </Content>
+        <Background />
+      </Container>
+    </>
+  );
+};
 
 export default SignIn;
